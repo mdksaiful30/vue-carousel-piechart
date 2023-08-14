9827b73f5ebe592932e27d31287655030bc64ef2
Md. Kamruzzaman <mdksaiful30@users.noreply.github.com>
2023-08-14 18:22:57 +0600

    changes welcome page to add URL

---
+++ TheWelcome.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineComponent } from 'vue'
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <h1 class="text-4xl font-bold text-gray-800 mt-5 mb-5">Welcome to Vue components assignment</h1>
+    <h3 class="text-2xl font-bold text-blue-500 mt-5 mb-5">To see the actual component, please navigate <span class="text-red-500">Carousel</span> and <span class="text-red-500">PieChart</span> on the above menu bar</h3>
+    <h4 class="text-gray-600 text-xl mt-5"> Please click to access github repository URL to assess assignment as Task1 and Task2</h4>
+    <div class="p-5 mt-10 bg-fuchsia-500">
+      <a class="text-4xl " href="https://github.com/mdksaiful30/vue-carousel-piechart" target="_blank">Github Repository Link >></a>
+    </div>
+  </div>
+</template>
